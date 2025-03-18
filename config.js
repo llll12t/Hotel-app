5276bd3d5bc13f18bd7e827b89235b111c5852c2
llll12t <147978645+llll12t@users.noreply.github.com>
2025-03-18 17:01:55 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// กำหนด API URL (URL นี้ใช้สำหรับ GET และ POST ทั้งคู่)
+const API_URL = "https://script.google.com/macros/s/AKfycbz6M0KzJZBVV3r61igQTvucTn2JTKJK965xVjDdQ7fRXSh_MkG6SVzaSH3MFnyxuFC8/exec";
