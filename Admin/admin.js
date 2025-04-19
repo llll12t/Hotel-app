bac2d5c8f5c8e1b0917b27ee2ad9e60b62a5b96a
llll12t <147978645+llll12t@users.noreply.github.com>
2025-04-20 00:17:21 +0700

    Update admin.js

--- Admin/admin.js
+++ Admin/admin.js
@@ -1,1 +1,4 @@
+ const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyPljZ05WrGsoQK08sjgbt3MRD9TO1s12jVrTO5Py_3-WhyyHjIsdiOA0nDhdD34tCV/exec'; //dash
+ const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxLSFeNkETYofdpZVeMQGIUGNVApIlMoY38IDmpqXKDG-FCrSZ4k3_IzI3LAR7i8_2Fpw/exec"; //member
+ const API_URL = "https://script.google.com/macros/s/AKfycbxLx5_3Gb7PwMLAH-ngFVWFebOSItk5MwzgzHv1W0k8fCbGY1-gJAdqk7vSvFgwLnoz/exec"; //room
 
