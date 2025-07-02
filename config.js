b21acee90317cd906f7eab1e6a55f004582b0211
llll12t <147978645+llll12t@users.noreply.github.com>
2025-07-02 10:20:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
-// กำหนด API URL (URL นี้ใช้สำหรับ GET และ POST ทั้งคู่)
-const API_URL = "https://script.google.com/macros/s/AKfycbz6M0KzJZBVV3r61igQTvucTn2JTKJK965xVjDdQ7fRXSh_MkG6SVzaSH3MFnyxuFC8/exec";
-const DAPI_URL = "https://script.google.com/macros/s/AKfycbzCetpojoUtgjAmmzHChAkmIHep68FGBJIflifNo1snLi7DhurS6INxKAlNwfkv-Kar/exec";
+
+// config.js
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbz6M0KzJZBVV3r61igQTvucTn2JTKJK965xVjDdQ7fRXSh_MkG6SVzaSH3MFnyxuFC8/exec',
+  GAS_URL_ADMINM: 'https://script.google.com/macros/s/AKfycbzCetpojoUtgjAmmzHChAkmIHep68FGBJIflifNo1snLi7DhurS6INxKAlNwfkv-Kar/exec',
+  GAS_URL_ADMIN: 'https://script.google.com/macros/s/AKfycbzZ7QNIgh1I8mm0khQ4j-y2G0BGiUjHO3obmsnTokdSaencO3-BzPHUKIS8F6rP0Xf3OQ/exec' ,
+  
+  LIFF_ID: '2007106715-87nBmwNL',
+  LIFF_HISTORY:'2007106715-OYJEx946'
+};
 
