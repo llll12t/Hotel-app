b209fa779a74742080cbe335dd637a13d80f04fd
llll12t <147978645+llll12t@users.noreply.github.com>
2025-07-02 10:10:03 +0700

    Create Config.js

---
+++ Admin/Config.js
@@ -0,0 +1,8 @@
+// config.js
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxEVn4eN8gQwYnfoAFOoMGnN60v5LwnuZBOApny5X8msERg7Mij-6CN7cMa77QX0_Pd/exec',
+  GAS_URL_ADMIN: 'https://script.google.com/macros/s/AKfycbzZ7QNIgh1I8mm0khQ4j-y2G0BGiUjHO3obmsnTokdSaencO3-BzPHUKIS8F6rP0Xf3OQ/exec' ,
+  
+  LIFF_ID: '2007514355-PqdpWXQ4',
+  LIFF_REGISTER_ID:'2007514355-6vPNByJ5'
+};
